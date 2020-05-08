8e0e2528243ed1e813c5a377a7479e2747c5cb4e
80036nd <40246027+80036nd@users.noreply.github.com>
2020-05-08 11:10:17 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/Documents/RestoreM8
+chmod 777 futurerestore
 ./futurerestore -t blob.shsh2 --latest-sep --latest-baseband restore.ipsw
 clear
 echo "Your Device Should be Restored"
